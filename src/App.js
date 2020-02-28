d00a517bb6af8836e83ca60f4d8cb654d3164b4f
ardabudi <ardabudi9@gmail.com>
2020-02-28 22:47:29 +0700

    Posapp-usingReactjs

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import './App.css'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Products from './components/home/Products'
+import Login from './components/Login'
+
+class App extends Component {
+  render () {
+    return (
+      <Router>
+        <Route path='/' exact component={Products} />
+        <Route path='/login' component={Login} />
+      </Router>
+    )
+  }
+}
+
+export default App
